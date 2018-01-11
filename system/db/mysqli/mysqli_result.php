a31f9e7adda5ac280973b85be4f73b269d927eaf
tzq <290847350@qq.com>
2018-01-11 11:36:24 +0800

    mysql类方法和所需要的配置文件

--- system/db/mysqli/mysqli_result.php
+++ system/db/mysqli/mysqli_result.php
@@ -28,4 +28,33 @@ class CMysqli_result extends CResult {
             $this->resultobj->free();
         }
     }
+    /**
+     * 返回数据表的单个字段的一维数组数据
+     * @param string $field 返回的字段名称,可选参数，为空就默认返回第一个字段的数组
+     * @param string $key 作为键的字段名称，可选参数，为空就默认数字键
+     * @return mixed
+     */
+    public function list_field($field = '', $key = '') {
+        if(empty($this->resultobj) || !is_object($this->resultobj)) {
+            return false;
+        }
+        $ret = array();
+        if (!empty($field)) {
+            if (!empty($key)) {
+                while($row = $this->resultobj->fetch_array(MYSQLI_ASSOC)) {
+                    $ret[$row[$key]] = $row[$field];
+                }
+                return $ret;
+            }
+            while($row = $this->resultobj->fetch_array(MYSQLI_ASSOC)) {
+                $ret[] = $row[$field];
+            }
+            return $ret;
+        }
+
+        while($row = $this->resultobj->fetch_array(MYSQLI_NUM)) {
+            $ret[] = $row[0];
+        }
+        return $ret;
+    }
 }
